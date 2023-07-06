35b4adc98f8c7a8ddd217a2c5dffe36373c2645d
csargin <120469159+csargin@users.noreply.github.com>
2023-07-06 11:33:25 +0300

    Update count_letters.py

--- count_letters.py
+++ count_letters.py
@@ -1,12 +1,20 @@
-def unique_english_letters(word):
+def count_char_x(word,l=""):
     letter_dict = {}
 
     for letter in word:
-        if letter not in letter_dict.keys():
+        if letter not in letter_dict:
             letter_dict[letter] = 1
         else:
             letter_dict[letter] += 1
-    return letter_dict
+    if l == "":
+        return len(letter_dict)
+    else:
+        return (letter_dict[l])
 
 
-print(unique_english_letters("mississippi"))
+print(count_char_x("mississippi", "s"))
+# should print 4
+print(count_char_x("mississippi", "m"))
+# should print 1
+print(count_char_x("mississippi"))
+# should print 4
